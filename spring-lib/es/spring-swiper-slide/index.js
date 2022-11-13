632022ddefd5e68d606892bf8663972563683776
shaochuwang <shaochuwang@tencent.com>
2022-11-13 22:26:00 +0800

    chore: build

---
+++ spring-lib/es/spring-swiper-slide/index.js
@@ -0,0 +1,7 @@
+import './src/iwan-swiper-slide.vue.js';
+import script from './src/iwan-swiper-slide.vue_vue_type_script_lang.vue.js';
+export { default } from './src/iwan-swiper-slide.vue_vue_type_script_lang.vue.js';
+
+script.install = (Vue) => {
+    Vue.component(script.name, script);
+  };
